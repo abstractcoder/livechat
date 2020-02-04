9819bacfd4b68cad2182aa6bb5b689bafd45db27
Steve Johnson <steve@abstractcoder.com>
2020-02-04 16:18:48 -0600

    Implemented version 1 of stimulus messages controller

--- assets/js/controllers/hello_controller.js
+++
@@ -1,7 +0,0 @@
-import { Controller } from "../../node_modules/stimulus"
-
-export default class extends Controller {
-  connect() {
-    console.log("Hello, Stimulus!", this.element)
-  }
-}
---
+++ assets/js/controllers/messages_controller.js
@@ -0,0 +1,67 @@
+import { Controller } from "stimulus"
+
+export default class extends Controller {
+  static targets = ["messageAlert", "messages"]
+
+  connect() {
+    this.scrollToBottom(false)
+
+    this.element.addEventListener("scroll", () => this.scrollHandler())
+
+    const callback = (mutationsList, observer) => {
+      for (let mutation of mutationsList) {
+        for (let node of mutation.addedNodes) {
+          this.showMessage(node)
+        }
+      }
+    }
+    this.observer = new MutationObserver(callback)
+    this.observer.observe(this.messagesTarget, { childList: true })
+  }
+
+  disconnect() {
+    this.element.removeEventListener("scroll", () => this.scrollHandler())
+    this.observer.disconnect()
+  }
+
+  showMessageAlert() {
+    this.messageAlertTarget.removeAttribute("hidden")
+  }
+
+  hideMessageAlert() {
+    this.messageAlertTarget.setAttribute("hidden", true)
+  }
+
+  scrollHandler() {
+    if (this.element.scrollHeight - this.element.offsetHeight - this.element.scrollTop < 10) {
+      this.hideMessageAlert() 
+      this.atBottom = true
+    }
+    else {
+      this.atBottom = false
+    }
+  }
+
+  scrollToBottom(smooth = true) {
+    let opts = { top: this.element.scrollHeight }
+
+    if (smooth) {
+      opts["behavior"] = "smooth"
+    }
+
+    this.element.scrollTo(opts)
+
+    this.atBottom = true
+  }
+
+  showMessage(node) {
+    if (this.atBottom) {
+      this.scrollToBottom()
+    }
+    else {
+      this.showMessageAlert()
+      this.element.scrollBy(0, -node.clientHeight)
+    }
+  }
+
+}
