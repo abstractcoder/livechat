b51097d19ae6d13997238bc3041e90803d4b8bd8
Steve <steve@abstractcoder.com>
2020-02-06 13:23:31 -0600

    Update Trix editor when live view updates content input, autofocus Trix editor

--- assets/js/app.js
+++ assets/js/app.js
@@ -19,8 +19,19 @@ import "phoenix_html"
 import {Socket} from "phoenix"
 import LiveSocket from "phoenix_live_view"
 
+let Hooks = {}
+Hooks.Trix = {
+  updated() {
+    const trix = document.querySelector(`trix-editor[input=${this.el.id}]`)
+    if (trix) {
+      trix.value = this.el.value
+      trix.focus()
+    }
+  }
+}
+
 let csrfToken = document.querySelector("meta[name='csrf-token']").getAttribute("content");
-let liveSocket = new LiveSocket("/live", Socket, {params: {_csrf_token: csrfToken}});
+let liveSocket = new LiveSocket("/live", Socket, {params: {_csrf_token: csrfToken}, hooks: Hooks});
 liveSocket.connect()
 
 require("../node_modules/trix/dist/trix")
