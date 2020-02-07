7e7f6fc644795bcc66b63800c10e6ed77e3f2a5f
Steve Johnson <steve@abstractcoder.com>
2020-02-07 17:32:38 -0600

    Added date divider

--- lib/livechat_web/views/chat_view.ex
+++ lib/livechat_web/views/chat_view.ex
@@ -3,7 +3,33 @@ defmodule LivechatWeb.ChatView do
 
   alias Livechat.Chat.Message
 
+  use Timex
+
   def edited?(%Message{} = message) do
     message.inserted_at != message.updated_at
+  end
+
+  def day_changed?(new_date, nil), do: true
+
+  def day_changed?(new_message, old_message) do
+    new_message.inserted_at.day != old_message.inserted_at.day
+  end
+
+  def format_time(datetime) do
+    datetime
+    |> Timex.to_datetime(Timex.Timezone.Local.lookup())
+    |> Timex.format!("%l:%M%P", :strftime)
+  end
+
+  def format_datetime(datetime) do
+    datetime
+    |> Timex.to_datetime(Timex.Timezone.Local.lookup())
+    |> Timex.format!("%l:%M%P on %A, %B %e", :strftime)
+  end
+
+  def format_date(datetime) do
+    datetime
+    |> Timex.to_datetime(Timex.Timezone.Local.lookup())
+    |> Timex.format!("%A, %B %e", :strftime)
   end
 end
--- mix.exs
+++ mix.exs
@@ -44,7 +44,8 @@ defmodule Livechat.MixProject do
       {:jason, "~> 1.0"},
       {:plug_cowboy, "~> 2.0"},
       {:phoenix_live_view, "~> 0.6"},
-      {:phoenix_slime, "~> 0.13.1"}
+      {:phoenix_slime, "~> 0.13.1"},
+      {:timex, "~> 3.5"}
     ]
   end
 
